b2cb70585dfec4719c79f96ca4891d96fa9f6ee0
firas hamila <firas.hm.1996@gmail.com>
2025-04-15 19:48:41 +0200

    aaa

--- controllers/userController.js
+++ controllers/userController.js
@@ -14,3 +14,34 @@ exports.createUser = async (req, res) => {
     });
   }
 };
+
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.status(200).json({
+      message: "Useres Fetched !!!",
+      nbr: users.length,
+      data: { users },
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!!!",
+      error: error,
+    });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    res.status(200).json({
+      message: "Useres Fetched !!!",
+      data: { user },
+    });
+  } catch (error) {
+    res.status(400).json({
+      message: "Fail !!!!",
+      error: error,
+    });
+  }
+};
